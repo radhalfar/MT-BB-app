052bab624e8b1c6b79f1d14f2a9df1056d4bd1e7
Radim Halfar <raradi@seznam.cz>
2024-01-25 22:59:13 +0100

    Updated fragments

--- app/src/main/java/cz/fai/utb/mt_bb_app/ui/favourites/FavouritesViewModel.kt
+++ app/src/main/java/cz/fai/utb/mt_bb_app/ui/favourites/FavouritesViewModel.kt
@@ -1,13 +1,13 @@
-package cz.fai.utb.mt_bb_app.ui.dashboard
+package cz.fai.utb.mt_bb_app.ui.favourites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavouritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/cz/fai/utb/mt_bb_app/ui/search/SearchViewModel.kt
+++ app/src/main/java/cz/fai/utb/mt_bb_app/ui/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package cz.fai.utb.mt_bb_app.ui.notifications
+package cz.fai.utb.mt_bb_app.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
