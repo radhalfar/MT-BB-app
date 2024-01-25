a175949cf384337b381dd7dc1c563aa09e3ebbde
Radim Halfar <raradi@seznam.cz>
2024-01-25 19:02:54 +0100

    Prepared layout

--- app/src/main/java/cz/fai/utb/mt_bb_app/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/cz/fai/utb/mt_bb_app/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import cz.fai.utb.mt_bb_app.databinding.FragmentDashboardBinding
+import cz.fai.utb.mt_bb_app.databinding.FragmentSearchBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentSearchBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
