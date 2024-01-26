9898ae56263a7ff1b8aec19cecb12f05e90fb2f0
Radim Halfar <raradi@seznam.cz>
2024-01-26 04:06:53 +0100

    Added icons

--- app/src/main/java/cz/fai/utb/mt_bb_app/ui/home/HomeViewModel.kt
+++ app/src/main/java/cz/fai/utb/mt_bb_app/ui/home/HomeViewModel.kt
@@ -1,44 +1,55 @@
 package cz.fai.utb.mt_bb_app.ui.home
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cz.fai.utb.mt_bb_app.Repository
 import cz.fai.utb.mt_bb_app.model.BreakingBadQuote
-import cz.fai.utb.mt_bb_app.model.BreakingBadQuoteList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+
+
 
 class HomeViewModel(private val repository: Repository) : ViewModel() {
     private val _breakingBadQuote = MutableLiveData<BreakingBadQuote>()
     val breakingBadQuote : LiveData<BreakingBadQuote> = _breakingBadQuote
 
-    /*
-    private val _text = MutableLiveData<String>().apply {
-        value = breakingBadQuote.value?.quote;
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name="Favourites")
+
+
+
+    init {
+
     }
-    val text: LiveData<String> = _text*/
 
     @SuppressLint("NullSafeMutableLiveData")
     fun getBBQuote() {
         viewModelScope.launch(Dispatchers.IO) {
             val result = repository.getBBQuote()
 
-            var msg = result?.quote;
-
-            if(result == null)
-                msg = "null"
-            else
-                msg = result.quote
-
-            Log.d("LOL", msg)
             if (result is BreakingBadQuote) {
                 _breakingBadQuote.postValue(result);
 
             }
+        }
+    }
+
+    fun addToFavQuotes() {
+        viewModelScope.launch(Dispatchers.IO) {
+           /*context.dataStore.edit { favourites ->
+               val currentCounterValue = favourites["pocitadlo"] ?: 0
+               favourites["pocitadlo"] = currentCounterValue + 1
+
+           }
+
+            Log.d("FavoriteQuotes", "All Favorite Quotes:\n$allQuotes")*/
         }
     }
 }
